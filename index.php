d981a873745d85326c31026a33c4d17e82bef0f1
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-03-23 23:47:53 +0200

    create base html template in php index file

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+header('Content-type: text/html; charset=utf-8');
+error_reporting(-1);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Guests Book</title>
+</head>
+<body>
+  <form action="index.php" method="POST">
+    <p>
+      <label for="name">Имя: </label><br>
+      <input type="text" name="name" id="name">
+    </p>
+    <p>
+      <label for="text">Текст: </label><br>
+      <textarea name="name" id="text"></textarea>
+    </p>
+    <button type="submit">Написать</button>
+  </form>
+</body>
+</html>
